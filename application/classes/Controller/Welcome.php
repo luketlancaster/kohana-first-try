1d6f81128d20a905f7df8abefa470c7e3731fa9a
Luke Lancaster <luketlancaster@gmail.com>
2015-12-16 23:00:42 -0500

    initial commit of first kohana app

---
+++ application/classes/Controller/Welcome.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Welcome extends Controller {
+
+	public function action_index()
+	{
+    $view = View::factory('index');
+		$this->response->body($view);
+	}
+
+} // End Welcome
---
+++ application/views/index.php
@@ -0,0 +1,27 @@
+<head>
+
+  <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+  <style>
+      body {
+        font-family: 'Lato', sans-serif;
+        text-align: center;
+        font-size: 80px;
+      }
+
+      h1 {
+        font-size: 1em;
+      }
+
+      h2 {
+        font-size: .5em;  
+      }
+
+  </style>
+</head>
+
+
+<h1>Hello World</h1>
+
+
+<h2>Brough to you buy Kohana</h2>
